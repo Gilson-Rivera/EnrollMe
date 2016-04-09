4ad575bc14ca5dd1a76671b0116b896a1a333bce
Diego Figueroa <diego.fv94@gmail.com>
2016-04-09 13:24:18 -0400

    modify token list - change tokens object from set to list - remove unnecessary tokens and rules - add course and section rule

--- EnrollMeLex.py
+++ EnrollMeLex.py
@@ -13,29 +13,14 @@ reserved = {
 }
 
 # List of token names. This is always required
-tokens = (
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-)
+tokens = [
+    'COURSE',
+    'SECTION'
+]
 
 # Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+t_COURSE = r'[a-zA-z]{4}[0-9]{4}'
+t_SECTION = r'[0-9]{3}'
 
 # Define a rule so we can track line numbers
 def t_newline(t):
