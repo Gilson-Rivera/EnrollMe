bf3e775d4b896e14468777b746dbfac4f0928117
Diego Figueroa <diego.fv94@gmail.com>
2016-04-11 10:13:45 -0400

    fix parsing rules - add missing whitespace between grammar statements and rules - change DROP statement rule to go according to design - add debugging print statements for clarity

--- EnrollMeYacc.py
+++ EnrollMeYacc.py
@@ -11,16 +11,17 @@ from EnrollMeLex import tokens
 
 # Primary reduce statement
 def p_statement(p):
-    '''statement: statement_enroll
-                | statement_drop
-                | statement_change
-                | statement_available
-                | statement_schedule'''
+    '''statement : statement_enroll
+                 | statement_drop
+                 | statement_change
+                 | statement_available
+                 | statement_schedule'''
+    print("parsed 'statement'")
     pass
 
 
 def p_statement_enroll(p):
-    'statement_enroll: ENROLL COURSE SECTION'
+    'statement_enroll : ENROLL COURSE SECTION'
     p[0] = p[1] + p[2] + p[3]
     # Substitute for real implementation, which should call
     # methods defined in imported module.
@@ -29,28 +30,39 @@ def p_statement_enroll(p):
     print("tokens: " + p[2] + " " + p[3])
 
 def p_statement_drop(p):
-    'statement_drop: DROP COURSE SECTION'
-    p[0] = p[1] + p[2] + p[3]
+    'statement_drop : DROP COURSE'
+    p[0] = p[1] + p[2]
     # Same
     print("parsed 'statement_drop'")
     print("identifier: " + p[1])
-    print("tokens: " + p[2] + " " + p[3])
+    print("tokens: " + p[2])
 
 def p_statement_change(p):
-    'statement_change: CHANGE COURSE SECTION'
+    'statement_change : CHANGE COURSE SECTION'
     p[0] = p[1] + p[2] + p[3]
     # Same
-    print("parsed 'change_statement'")
+    print("parsed 'statement_change'")
     print("identifier: " + p[1])
     print("tokens: " + p[2] + " " + p[3])
 
 def p_statement_available(p):
-    'statement_available: AVAILABLE COURSE PROFESSOR TIME'
-    p[0] = p[1] + p[2] + p[3]
+    '''statement_available : AVAILABLE COURSE PROFESSOR TIME
+                         | AVAILABLE COURSE PROFESSOR
+                         | AVAILABLE COURSE TIME
+                         | AVAILABLE PROFESSOR TIME
+                         | AVAILABLE COURSE
+                         | AVAILABLE PROFESSOR
+                         | AVAILABLE TIME
+                         | AVAILABLE'''
     # Same
+    print("parsed 'statement_available'")
+    print("identifier: " + p[1])
+    print("tokens: ")
+    for part in p[2:]:
+        print(part)
 
 def p_statement_schedule(p):
-    'statement_schedule: SCHEDULE'
+    'statement_schedule : SCHEDULE'
     p[0] = p[1]
     # Same
 
