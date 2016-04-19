02152efd8b10194a18005510b377f3eff9a0a998
Diego Figueroa <diego.fv94@gmail.com>
2016-04-19 14:34:14 -0400

    create EnrollMeTools module with initial signatures

---
+++ EnrollMeTools.py
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------
+# EnrollMeTools.py
+# ------------------------------------------------------------
+
+# enroll(course, section)
+# Pre-Requisites:
+#    * User must be logged in
+#    * course must be valid
+#    * section must be valid
+def enroll(course, section):
+
+# drop(course)
+# Pre-Requisites:
+#    * User must be logged in
+#    * course must be valid
+
+# change(course, section)
+# Pre-Requisites:
+#    * User must be logged in
+#    * course must be valid
+#    * section must be valid
+
+# available(course, professor, time)
+# Pre-Requisites:
+#    * User must be logged in
+#    * course, professor and time are optional
+
+# schedule()
+# Pre-Requisites:
+#    * User must be logged in
