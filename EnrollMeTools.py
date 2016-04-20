6ca72f2912a42368a2345cd74020191549d5eaf9
Diego Figueroa <diego.fv94@gmail.com>
2016-04-20 11:46:08 -0400

    add better description of functions and initial drop implementation

--- EnrollMeTools.py
+++ EnrollMeTools.py
@@ -26,6 +26,14 @@ def enroll(course, section):
         return 'Successfully enrolled in ' + course.upper() + ' ' + section
 
 def drop(course):
+    payload = {'studentID': credentials, 'courseID': course}
+    r = requests.delete('http://162.243.3.45/EnrollMeAPI/api/public/v1/usercourses', params=payload)
+    if(r.status_code == 404):
+        message = r.json()
+        message = message['message']
+        return message
+    else:
+        return 'Successfully dropped of ' + course.upper()
 
 def change(course, section):
     payload = {'studentID': credentials, 'courseName': course}
@@ -41,13 +49,13 @@ def change(course, section):
     if(r.status_code == 200):
         return 'Successfully changed to ' + course.upper() + ' ' + section
 
-# available(course, professor, time)
-# Pre-Requisites:
-#    * User must be logged in
-#    * course, professor and time are optional
-def available(course, professor, time):
+# Requisites:
+#   Course, Professor and Time are optional;
+#   check for existence of input variables, and
+#   construct request URL accordingly before query.
+# def available(course, professor, time):
 
-# schedule()
-# Pre-Requisites:
-#    * User must be logged in
-def schedule():
+# Requisites:
+#   Query for all courses in schedule, and
+#   draw a table accordingly.
+# def schedule():
