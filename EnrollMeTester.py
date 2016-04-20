e3c95cda4cdbe43fe6ca812f7d50f7377d2b4b34
janvega1 <jan.vega1@upr.edu>
2016-04-20 15:52:52 -0400

    fix order of test case arguments

--- EnrollMeTester.py
+++ EnrollMeTester.py
@@ -11,45 +11,45 @@ class MyTestFunctions(unittest.TestCase):
     def test_valid_ID(self):
 
         lexer.input("enroll")
-        self.assertMultiLineEqual(str(lexer.token()), "LexToken(ENROLL,'enroll',1,0)")
+        self.assertMultiLineEqual("LexToken(ENROLL,'enroll',1,0)", str(lexer.token()))
 
         lexer.input("drop")
-        self.assertEqual(str(lexer.token()), "LexToken(DROP,'drop',1,0)")
+        self.assertEqual("LexToken(DROP,'drop',1,0)", str(lexer.token()))
 
         lexer.input("change")
-        self.assertEqual(str(lexer.token()), "LexToken(CHANGE,'change',1,0)")
+        self.assertEqual("LexToken(CHANGE,'change',1,0)", str(lexer.token()))
 
         lexer.input("available")
-        self.assertEqual(str(lexer.token()), "LexToken(AVAILABLE,'available',1,0)")
+        self.assertEqual("LexToken(AVAILABLE,'available',1,0)", str(lexer.token()))
 
         lexer.input("schedule")
-        self.assertEqual(str(lexer.token()), "LexToken(SCHEDULE,'schedule',1,0)")
+        self.assertEqual("LexToken(SCHEDULE,'schedule',1,0)", str(lexer.token()))
 
 
     def test_enroll_a_course(self):
         lexer.input("enroll ICOM4036")
 
-        self.assertEqual(str(lexer.token()), "LexToken(ENROLL,'enroll',1,0)")
-        self.assertEqual(str(lexer.token()), "LexToken(COURSE,'ICOM4036',1,7)")
+        self.assertEqual("LexToken(ENROLL,'enroll',1,0)", str(lexer.token()))
+        self.assertEqual("LexToken(COURSE,'ICOM4036',1,7)", str(lexer.token()))
 
     def test_drop_a_course(self):
         lexer.input("drop ICOM4036 030")
 
-        self.assertMultiLineEqual(str(lexer.token()), "LexToken(DROP,'drop',1,0)")
-        self.assertEqual(str(lexer.token()), "LexToken(COURSE,'ICOM4036',1,5)")
-        self.assertEqual(str(lexer.token()), "LexToken(SECTION,'030',1,14)")
+        self.assertMultiLineEqual("LexToken(DROP,'drop',1,0)", str(lexer.token()))
+        self.assertEqual("LexToken(COURSE,'ICOM4036',1,5)", str(lexer.token()))
+        self.assertEqual("LexToken(SECTION,'030',1,14)", str(lexer.token()))
 
     def test_change_a_course(self):
         lexer.input("change ICOM5995")
 
-        self.assertEqual(str(lexer.token()), "LexToken(CHANGE,'change',1,0)")
-        self.assertEqual(str(lexer.token()), "LexToken(COURSE,'ICOM5995',1,7)")
+        self.assertEqual("LexToken(CHANGE,'change',1,0)", str(lexer.token()))
+        self.assertEqual("LexToken(COURSE,'ICOM5995',1,7)", str(lexer.token()))
 
     def test_schedule(self):
         lexer.input("schedule morning")
 
-        self.assertEqual(str(lexer.token()), "LexToken(SCHEDULE,'schedule',1,0)")
-        self.assertEqual(str(lexer.token()), "LexToken(TIME,'morning',1,9)")
+        self.assertEqual("LexToken(SCHEDULE,'schedule',1,0)", str(lexer.token()))
+        self.assertEqual("LexToken(TIME,'morning',1,9)", str(lexer.token()))
 
 
     if __name__ == '__main__':
