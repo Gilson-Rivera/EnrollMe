0ff5fc4e831d6359d34f8570edae697d1f100726
Diego Figueroa <diego.fv94@gmail.com>
2016-04-19 22:30:34 -0400

    add other reserved word signatures

--- EnrollMeTools.py
+++ EnrollMeTools.py
@@ -13,18 +13,22 @@ def enroll(course, section):
 # Pre-Requisites:
 #    * User must be logged in
 #    * course must be valid
+def drop(course):
 
 # change(course, section)
 # Pre-Requisites:
 #    * User must be logged in
 #    * course must be valid
 #    * section must be valid
+def change(course, section):
 
 # available(course, professor, time)
 # Pre-Requisites:
 #    * User must be logged in
 #    * course, professor and time are optional
+def available(course, professor, time):
 
 # schedule()
 # Pre-Requisites:
 #    * User must be logged in
+def schedule():
